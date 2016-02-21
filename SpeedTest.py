881469ec9315f6e4e74c4567e605af3b7466e7c3
Nelly Liu Peng <nliupeng@gmail.com>
2016-02-21 00:14:07 -0800

    delete function optimized

--- SpeedTest.py
+++ SpeedTest.py
@@ -16,7 +16,7 @@ print("Dropbox now ready...")
 
 # UPLOADING FILE
 start = time()
-filename = 'testing.txt'
+filename = 'TEST.txt'
 f = open(filename, 'rb')
 response = client.put_file(filename, f)
 print("Upload complete!")
@@ -26,26 +26,7 @@ print('Elapsed time:', elapse)
 # DELETING FILE
 folder_metadata = client.metadata('/')          #displays relevant info in dropbox
 contents_list = folder_metadata['contents']     #this contains all files in dropbox. contents_list is a List of dicts
-
-def search(filename, path):             #search thru dict (this method used only if there were already files in dropbox)
-    for items in contents_list:         #(this method not needed if dropbox is empty from start->only one file present, no need to search)
-        if items['path'] == path:
-            return items
 path = '/%s'%(filename)
-justaddedfile = search(filename,path)   #just addedfile is a dict
-
-def delete(filename):
-    client.file_delete(path)
-delete(justaddedfile)
+client.file_delete(path)
 print("File deleted!")
-
-""" Replace current search() and delete() for this one if only one file in dropbox.
-Ideal to use when dropbox is initially empty. after a file is uploaded, immediately delete it.
-I commented this temporarily so we can keep on testing.
-
-def delete():
-    for item in contents_list:
-        delete(item)
-delete()
-"""
 
